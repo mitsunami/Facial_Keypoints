d735dda3a02a16a1e4136c5108a870daa42d9866
Koki Mitsunami <k.mitsunami@gmail.com>
2018-08-04 00:17:24 +0900

    update models

--- models.py
+++ models.py
@@ -21,13 +21,16 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         #self.conv1 = nn.Conv2d(1, 32, 5)
-        self.conv1 = nn.Conv2d(  1,  32, 4)
-        self.conv2 = nn.Conv2d( 32,  64, 3)
-        self.conv3 = nn.Conv2d( 64, 128, 2)
-        self.conv4 = nn.Conv2d(128, 256, 1)
+        self.conv1 = nn.Conv2d(  1,   8, 3)
+        self.conv2 = nn.Conv2d(  8,  16, 3)
+        self.conv3 = nn.Conv2d( 16,  32, 3)
+        self.conv4 = nn.Conv2d( 32,  64, 3)
+        self.conv5 = nn.Conv2d( 64, 128, 3)
         self.pool  = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(256, 136)
+        self.fc1 = nn.Linear(128*5*5, 1024)
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc3 = nn.Linear( 256, 136)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -41,8 +44,12 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
         x = self.pool(F.relu(self.conv4(x)))
-        x = x.view(-1, 256)
-        x = self.fc1(x)
+        x = self.pool(F.relu(self.conv5(x)))
+        #print(x.size())
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
 
         # a modified x, having gone through all the layers of your model, should be returned
         return x
